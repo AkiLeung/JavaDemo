a8d545371460bd64b0b83a5e84a28f9a03fe95de
AkiLeungQ <382926924@qq.com>
2019-04-11 19:25:46 -0700

    leetCode testing

--- src/main/java/com/demo/leetcode/ReverseNumber.java
+++ src/main/java/com/demo/leetcode/ReverseNumber.java
@@ -3,7 +3,7 @@ package com.demo.leetcode;
 /**
  * @author Joseph.L
  * @date 2019-04-12
- * @description 反正号码
+ * @description 反转号码
  */
 public class ReverseNumber {
 
@@ -11,14 +11,18 @@ public class ReverseNumber {
      * 主程序入口
      */
     public static void main(String[] args) {
-        int number = -4567;
-        System.out.println("反转结果：" + solution1(number));
+        int number = -405600;
+        System.out.println("方法1，反转结果：" + solution1(number));
+        System.out.println("方法2，反转结果：" + solution2(number));
     }
 
     /**
      * 方法1
+     * 时间复杂度：N，但因为循环中使用了String，GC介入因此慢
      */
     public static int solution1(int number) {
+        long startTime = System.currentTimeMillis();
+
         int intReturn = 0;
         double intMax = Math.pow(2, 31);
         double intMin = -Math.pow(2, 31);
@@ -40,7 +44,48 @@ public class ReverseNumber {
         if (number < 0) {
             intReturn = intReturn * -1;
         }
+        long endTime = System.currentTimeMillis();
+        long runTime = endTime - startTime;
+        System.out.println("方法1，运行时间：" + runTime);
         return intReturn;
     }
 
+    /**
+     * 方法2
+     * 时间复杂度：N*N,但因为避开循环中使用STRING，则避开使用GC介入，因此更快。
+     */
+    public static int solution2(int number) {
+        long startTime = System.currentTimeMillis();
+
+        int intReturn = 0;
+        double intMax = Math.pow(2, 31);
+        double intMin = -Math.pow(2, 31);
+        //校验
+        if (number == 0) {
+            return intReturn;
+        }
+        if (number > intMax || number < intMin) {
+            return intReturn;
+        }
+
+        String temp = "" + Math.abs(number);
+        int size = temp.length();
+        //反转数字
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = Integer.parseInt("" + temp.charAt(i));
+        }
+
+        for (int i = size - 1; i >= 0; i--) {
+            intReturn += array[i] * Math.pow(10,i);
+        }
+
+        if (number < 0) {
+            intReturn = intReturn * -1;
+        }
+        long endTime = System.currentTimeMillis();
+        long runTime = endTime - startTime;
+        System.out.println("方法2，运行时间：" + runTime);
+        return intReturn;
+    }
 }
