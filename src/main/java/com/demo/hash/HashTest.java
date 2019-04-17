51e7d5e80bbc3fdc5c595137b11c8b028efdae43
AkiLeungQ <382926924@qq.com>
2019-04-16 20:34:49 -0700

    Hash testing

--- src/main/java/com/demo/hash/HashTest.java
+++ src/main/java/com/demo/hash/HashTest.java
@@ -6,4 +6,15 @@ package com.demo.hash;
  * @description Test Hash Function
  */
 public class HashTest {
+
+    /**
+     * 主程序入口
+     * */
+    public static void main(String[] args) {
+        String strTemp1 = "Aa";
+        System.out.println(strTemp1.hashCode());
+        String strTemp2 = "BB";
+        System.out.println(strTemp2.hashCode());
+    }
+
 }
