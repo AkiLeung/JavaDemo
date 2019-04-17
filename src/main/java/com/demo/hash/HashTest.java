785419a5f5871c9fb0d3bf536ef4d44b978c9b7c
AkiLeungQ <382926924@qq.com>
2019-04-16 20:57:29 -0700

    Hash testing

--- src/main/java/com/demo/hash/HashTest.java
+++ src/main/java/com/demo/hash/HashTest.java
@@ -9,12 +9,23 @@ public class HashTest {
 
     /**
      * 主程序入口
-     * */
+     */
     public static void main(String[] args) {
-        String strTemp1 = "Aa";
-        System.out.println(strTemp1.hashCode());
-        String strTemp2 = "BB";
-        System.out.println(strTemp2.hashCode());
+        test01();
     }
 
+    /**
+     * 测试一
+     * */
+    public static void test01() {
+        String strTemp1 = "Aa";
+        String strTemp2 = "BB";
+        System.out.println(strTemp1.hashCode());
+        System.out.println(strTemp2.hashCode());
+        if (strTemp1.hashCode() == strTemp2.hashCode() && strTemp1 == strTemp2) {
+            System.out.println("变量相等");
+        } else {
+            System.out.println("变量不等");
+        }
+    }
 }
