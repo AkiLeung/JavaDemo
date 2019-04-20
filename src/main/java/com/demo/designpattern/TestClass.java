5e8c5e6c70c721e581a3c955878bf6848257541e
AkiLeungQ <382926924@qq.com>
2019-04-20 02:43:31 -0700

    encrypt and decrypt by DES

--- src/main/java/com/demo/designpattern/TestClass.java
+++ src/main/java/com/demo/designpattern/TestClass.java
@@ -1,4 +1,4 @@
-package com.demo.designPattern;
+package com.demo.designpattern;
 
 /**
  * @author Joseph.L
@@ -6,4 +6,21 @@ package com.demo.designPattern;
  * @description 测试
  */
 public class TestClass {
+
+    public static void main(String[] args) {
+
+        /*饿汉模式*/
+        Singleton1 s1 = Singleton1.getInstance();
+        Singleton1 s2 = Singleton1.getInstance();
+        if (s1 == s2) {
+            System.out.println("s1==s2");
+        }
+
+        /*懒汉模式*/
+        Singleton2 s3 = Singleton2.getInstance();
+        Singleton2 s4 = Singleton2.getInstance();
+        if (s3 == s4) {
+            System.out.println("s3==s4");
+        }
+    }
 }
---
+++ src/main/java/com/demo/encryption/EncryptDes.java
@@ -0,0 +1,55 @@
+package com.demo.encryption;
+
+import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.DESedeKeySpec;
+import java.security.Key;
+
+/**
+ * @author Joseph.L
+ * @date 2019-04-20
+ * @description DES对称加密
+ */
+public class EncryptDes {
+
+    private static String info = "测试加密信息 Test 123 ！@#￥%……&*（）——+";
+
+    public static void main(String[] args) {
+        jdkDES();
+    }
+
+    public static void jdkDES() {
+        try {
+            //生成KEY
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
+            keyGenerator.init(56);
+            SecretKey secretKey = keyGenerator.generateKey();
+            byte[] bytesKey = secretKey.getEncoded();
+
+            //KEY转换
+            DESKeySpec deSedeKeySpec = new DESKeySpec(bytesKey);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("DES");
+            Key convertSecretKey = factory.generateSecret(deSedeKeySpec);
+
+            //加密
+            Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE,convertSecretKey);
+            byte[] result = cipher.doFinal(info.getBytes());
+            System.out.println("jdk des encrypt:"+ HexBin.encode(result));
+
+            //解密.
+            cipher.init(Cipher.DECRYPT_MODE,convertSecretKey);
+            result = cipher.doFinal(result);
+            System.out.println("jdk des decrypt:"+ new String(result));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
